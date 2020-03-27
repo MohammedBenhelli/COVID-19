21e1e3ca2451f7a6e2e0dff30ca86f1456770fa9
mohammed <mohammed.benhelli@epitech.eu>
2020-03-27 04:22:23 +0100

    o

--- src/View/User/login.php
+++ src/View/User/login.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
 <form action="/MVC_PiePHP/index.php" method="post">
     <div class="container">
         <label for="email"><b>Email</b>
@@ -19,5 +9,3 @@
         <button type="submit">Login</button>
     </div>
 </form>
-</body>
-</html>
