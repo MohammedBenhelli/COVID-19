9be3d403af20874e6bad90f0cf175c298ff694ab
mohammed <mohammed.benhelli@epitech.eu>
2020-04-21 00:58:56 +0200

    symfony > laravel

--- MVC_My_Quizz/src/Controller/RegistrationController.php
+++ MVC_My_Quizz/src/Controller/RegistrationController.php
@@ -28,7 +28,7 @@ class RegistrationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
 
             // 4) save the User!
